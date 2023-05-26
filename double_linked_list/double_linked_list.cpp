619b4464883ce4086196e7c33df5e0b263ca59c3
dzulfaqaar <fikri6aul@gmail.com>
2023-05-26 11:10:29 +0700

    insert

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -40,5 +40,12 @@ void DoubleLinkedList::addNode() {
 	newNode->noMhs = nim; //step 2
 	newNode->name = nm; //step 2
 
-
+	/*insert a node in the beginning of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START: //step 3
+	}
 }
